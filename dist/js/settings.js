1f88f33e9a4ac083e41a65b2f5731e85f58ee938
ElismatiQ <elismatiq@gmail.com>
2013-07-10 17:34:09 -0400

    js.headlines - july 2013

---
+++ dist/js/settings.js
@@ -0,0 +1,24 @@
+/*!
+ *
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
+ *
+ * Version: v3.1.1-release Build: 2013-05-16 04:34 AM
+ *
+ */
+/*
+WET-BOEW-Settings
+ */
+var wet_boew_properties = {
+	/** global plugins are called via an array of dependency names **/
+	globals : ['deselectradio', 'datemodified']
+};
+
+/*
+Global overrides for individual components
+ */
+
+// Share widget
+var wet_boew_share = {
+	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
+};
